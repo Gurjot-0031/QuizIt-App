c919be8ecbdb81f8a62809b54a6473c501277b2e
gursimransingh <gursimran81@gmail.com>
2019-03-24 18:03:16 -0400

    added topic js

--- main/topic.js
+++ main/topic.js
@@ -3,6 +3,7 @@ function runFunction() {
     document.addEventListener('submit', function (e) {
         e.preventDefault();
         console.log("JEYA"+e.target);
-
+        //getting userType from local storage
+        const userType = JSON.parse(localStorage.getItem("userType"));
     })
 }
