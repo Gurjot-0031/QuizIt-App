38967c26b82281bfbdc61b73ed147518f61e8793
gursimransingh <gursimran81@gmail.com>
2019-03-26 21:06:27 -0400

    Final Commit for the day

--- game.js
+++ game.js
@@ -1,20 +1,18 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const choices = Array.from(document.getElementsByClassName("choice-text"));//Making the HTML collection of P tags as array of choice this is where we add the chpices
+console.log(choices);
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
 const loader = document.getElementById("loader");
 const game = document.getElementById("game");
-
-
-
-let currentQuestion = {};
+let currentQuestion = {};// Current question of the available question.
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [];
+let questions = []; //getting questions from the API.
 
 //I need category
 const categoryVal  = localStorage.getItem("categoryVal");
@@ -30,18 +28,24 @@ const urls = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&t
 fetch(
     url
 )
-    .then(res => {
+    .then(res => { //THIS IS THE RESPONSE FROMT THE SERVER which returns THE JSON OF THE OUTPUT
         return res.json();
     })
-    .then(loadedQuestions => {
+    .then(loadedQuestions => { //THIS OUPUT BASICALLY HAS ALL THE QUESTIONS, SO FOR EACH QUESTION WE DO STUFF
+        console.log(loadedQuestions);
         console.log(loadedQuestions.results);
-        questions = loadedQuestions.results.map(loadedQuestion => {
+        //ITERATING through an array of loadedquestions and transforming them.
+        questions = loadedQuestions.results.map(loadedQuestion => { //MAPPING THE JSON THAT WE got from the API into another format, that related to our HTML
+            //CREATING a formatted question OBJECT
             const formattedQuestion = {
-                question: loadedQuestion.question
+                question: decodeHtml(loadedQuestion.question),
+                type: loadedQuestion.type
             };
-
+    //CReating an answer choices array in the map to have all the incorrect answers
             const answerChoices = [...loadedQuestion.incorrect_answers];
+
             formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            //ADDING THE CORRECT anser inside the answer choices array
             answerChoices.splice(
                 formattedQuestion.answer - 1,
                 0,
@@ -49,10 +53,10 @@ fetch(
             );
 
             answerChoices.forEach((choice, index) => {
-                formattedQuestion["choice" + (index + 1)] = choice;
+                formattedQuestion["choice" + (index + 1)] = decodeHtml(choice);
             });
 
-            return formattedQuestion;
+            return formattedQuestion; //Returning the Formatted Question Object
         });
 
         startGame();
@@ -63,13 +67,14 @@ fetch(
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     console.log(categoryVal+categoryText+user+difficulty);
     questionCounter = 0;
     score = 0;
-    availableQuesions = [...questions];
+    availableQuesions = [...questions]; //spreading out the questions that we get from the API to available Question array.
+    console.log(availableQuesions);
     getNewQuestion();
     game.classList.remove("hidden");
     loader.classList.add("hidden");
@@ -86,21 +91,47 @@ getNewQuestion = () => {
     //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length); //Getting a quesition index to display
+
+    //getting the current question to DISPLAY  out of all of the available questions.
     currentQuestion = availableQuesions[questionIndex];
+    //storing the current question to display in currentQuestion Array.
     console.log(currentQuestion);
-    console.log(currentQuestion.answer);
     question.innerText = currentQuestion.question;
+//========THIS IS WHERE THE CHOICES ARE PRINTED ON THE OPTIONS.
+//Iterating through each choice .Here choice variable has the reference to a p tag in the HTML collection.
 
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
-    });
+    
+    //BASED ON THE TYPE OF QUESTION WE GET, UPDATING THE UI
+    console.log(currentQuestion.type);
+    
+    
+    if (currentQuestion.type==='boolean'){
+        document.getElementById("extra1").style.display = "none";
+        document.getElementById("extra2").style.display = "none";
+        let count = 1;
+        choices.forEach(choice=>{
+            choice.innerText = currentQuestion["choice"+count];
+            count++;
+        });
 
+    }
+    else {
+        document.getElementById("extra1").style.display = "flex";
+        document.getElementById("extra2").style.display = "flex";
+        choices.forEach(choice => {
+            const number = choice.dataset["number"]; //it will be either 1,2,3 or 4 from data-number property.
+            console.log(number);
+            choice.innerText = currentQuestion["choice" + number];
+        });
+
+    }
+    //REMOVING THE QUESTION, ONCE ASKED
     availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
+//ADDING LISTENERS TO ALL OF THE CHOICES..
 choices.forEach(choice => {
     choice.addEventListener("click", e => {
         if (!acceptingAnswers) return;
@@ -118,6 +149,7 @@ choices.forEach(choice => {
 
         selectedChoice.parentElement.classList.add(classToApply);
 
+        //choice.parentElement.classList.add("correct");
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
@@ -145,6 +177,7 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 
+
 document.getElementById("form").addEventListener('submit',function (e) {
 
     e.preventDefault();
@@ -152,27 +185,36 @@ document.getElementById("form").addEventListener('submit',function (e) {
 
     acceptingAnswers = false;
     const inputText = document.getElementById("textField");
+    if (inputText.value ==="")
+        alert("Answer must be filled out");
 
-   // if (!acceptingAnswers) return;
 
-   if ( inputText.value.toLowerCase().trim() ===currentQuestion["choice" + currentQuestion.answer].toLowerCase().trim()){
+    // if (!acceptingAnswers) return;
+
+    if ( inputText.value.toLowerCase().trim() ===currentQuestion["choice" + currentQuestion.answer].toLowerCase().trim()){
         console.log("Correct");
-       inputText.parentElement.classList.add("correct");
-       setTimeout(function () {
-           inputText.parentElement.classList.remove("correct");
-           getNewQuestion();
-       },1000);
+        inputText.parentElement.classList.add("correct");
+        setTimeout(function () {
+            inputText.parentElement.classList.remove("correct");
+            getNewQuestion();
+        },1000);
 
 
-   }
-   else {
-       inputText.parentElement.classList.add("incorrect");
-       setTimeout(function () {
-           inputText.parentElement.classList.remove("incorrect");
-           getNewQuestion();
-       },1000);
-   }
+    }
+    else {
+        inputText.parentElement.classList.add("incorrect");
+        setTimeout(function () {
+            inputText.parentElement.classList.remove("incorrect");
+            getNewQuestion();
+        },1000);
+    }
 
 
 
-})
+});
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
